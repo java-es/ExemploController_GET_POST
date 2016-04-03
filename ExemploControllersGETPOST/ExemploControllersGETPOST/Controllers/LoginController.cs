c99b55fd38108eb835a5022f03c8eced3977aee7
Wanderson Alves Rodrigues <wandersonallves@yahoo.com.br>
2016-04-03 14:20:45 -0300

    Exemplo de uso dos métodos GET e POST ASP.NET MVC

---
+++ ExemploControllersGETPOST/ExemploControllersGETPOST/Controllers/LoginController.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace ExemploControllersGETPOST.Controllers
+{
+    public class LoginController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult LoginGet(string email, string senha)
+        {
+            return Content("O Login é: " + email + " a senha é: " + senha);
+        }
+
+        [HttpPost]
+        public ActionResult LoginPost(string email, string senha)
+        {
+            return Content("O Login é: " + email + " a senha é: " + senha);
+        }
+    }
+}
---
+++ ExemploControllersGETPOST/ExemploControllersGETPOST/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ExemploControllersGETPOST.MvcApplication" Language="C#" %>
